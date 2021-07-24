36f8b93a649925f5aa53e097b49b05b0a5a1404d
stefanocudini <stefano.cudini@gmail.com>
2021-07-24 18:52:46 +0200

    clean

--- c5526_index.php
+++
@@ -1,8 +0,0 @@
-<?
-/* powered by Ultra Light Gallery v2.55 */
-/* proprietario: admin - 30/01/2009 03:43:40 */
-#die("Pagina in manutenzione");
-#exit(0);
-$index=true;
-@require_once('config.php');
-?>
